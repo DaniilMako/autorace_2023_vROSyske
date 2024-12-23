e6a67802c3aa38b8dbb476b6bba1dcd9bf7a920a
Daniil Makovetskii <d.makovetskii@g.nsu.ru>
2024-12-23 14:30:34 +0700

    sigh detect, intersection
    sigh detect: yolov5

--- autorace_core_vROSyske/launch/autorace_core.launch.py
+++ autorace_core_vROSyske/launch/autorace_core.launch.py
@@ -18,17 +18,17 @@ def generate_launch_description():
             os.path.join(pkg_autorace_camera, 'launch', 'extrinsic_camera_calibration.launch.py')))
     
     base_config = os.path.join(pkg_autorace_core, 'config', 'base.yaml')
-    # intersection_config = os.path.join(pkg_autorace_core, 'config', 'intersection.yaml')
+    intersection_config = os.path.join(pkg_autorace_core, 'config', 'intersection.yaml')
     # obstacles_config = os.path.join(pkg_autorace_core, 'config', 'obstacles.yaml')
 
 
-    # sign_detection = Node(
-    #     package = 'autorace_core_vROSyske',
-    #     executable = 'sign_detection',
-    #     name = 'sign_detection',
-    #     parameters=[
-    #         {'model_path': os.path.join(pkg_autorace_core, 'model')},
-    #         base_config])
+    sign_detection = Node(
+        package = 'autorace_core_vROSyske',
+        executable = 'sign_detection',
+        name = 'sign_detection',
+        parameters=[
+            {'model_path': os.path.join(pkg_autorace_core, 'model')},
+            base_config])
     
     lane_detect = Node(
         package = 'autorace_core_vROSyske',
@@ -42,21 +42,21 @@ def generate_launch_description():
         name = 'lane_follow',
         parameters = [base_config])
     
-    # robot_rotator = Node(
-    #     package = 'autorace_core_vROSyske',
-    #     executable = 'robot_rotator',
-    #     name = 'robot_rotator')
+    robot_rotator = Node(
+        package = 'autorace_core_vROSyske',
+        executable = 'robot_rotator',
+        name = 'robot_rotator')
     
     traffic_light = Node(
         package = 'autorace_core_vROSyske',
         executable = 'traffic_light',
         name = 'traffic_light')
     
-    # intersection = Node(
-    #     package = 'autorace_core_vROSyske',
-    #     executable = 'intersection',
-    #     name = 'intersection',
-    #     parameters = [intersection_config])
+    intersection = Node(
+        package = 'autorace_core_vROSyske',
+        executable = 'intersection',
+        name = 'intersection',
+        parameters = [intersection_config])
     
     # obstacles = Node(
     #     package = 'autorace_core_vROSyske',
@@ -69,13 +69,13 @@ def generate_launch_description():
         camera_calibration,
 
         # Постоянно работающие ноды
-        # sign_detection,
+        sign_detection,
         lane_detect,
         lane_follow,
-        # robot_rotator,
+        robot_rotator,
 
         # Ноды испытаний
         traffic_light,
-        # intersection,
-        # obstacles,
+        intersection,
+        obstacles,
     ])
